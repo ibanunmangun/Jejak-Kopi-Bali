f687a28d60af978a596185eb3cf82e85294453a6
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2021-04-12 18:06:21 +0700

    Add files via upload

---
+++ data/MeanCenter_3.js
@@ -0,0 +1,8 @@
+var json_MeanCenter_3 = {
+"type": "FeatureCollection",
+"name": "MeanCenter_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "MEAN_X": 115.26702269090318, "MEAN_Y": -8.2982329667787607 }, "geometry": { "type": "Point", "coordinates": [ 115.267022690903175, -8.298232966778761 ] } }
+]
+}
